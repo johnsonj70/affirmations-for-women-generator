c96697f9fdb86f584e410871e5a0e8e34103c4d6
Janice <johnson.janice30@yahoo.co.uk>
2025-06-29 17:04:14 +0100

    Removed console.log statements

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 function displayAffirmation(response) {
-	console.log("affirmation generated");
 	//Display the generated affirmation
 	new Typewriter("#affirmation", {
 		strings: response.data.answer,
@@ -15,20 +14,18 @@ function generateAffirmation(event) {
 
 	let affirmationType = document.querySelector("#user-instructions");
 	affirmationType = affirmationType.value;
-	console.log(affirmationType);
 	
 	 if (affirmationType === "daily") {
 	
 	//build API url
-	let apiKey = "00a6bfb9b6053b4664t55oaa8c181e51";
-	let context = "You are a world-class motivational coach, and I would like you to give words of affirmations.  Make sure you follow the user instructions.";
-	let prompt = "User instructions: Send me an affirmation for women only.  Include the affirmation only in your response, followed by a <br>, then add 'SheCodes AI' inside a <strong> element. ";
-	let url = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-	let affirmationElement = document.querySelector("#affirmation");
-	affirmationElement.classList.remove("hidden");
-	affirmationElement.innerHTML = `<div class="generating">⏳</div> generating your ${affirmationType} affirmation`;
-	axios.get(url).then(displayAffirmation);
-		console.log("generating affirmation");
+		let apiKey = "00a6bfb9b6053b4664t55oaa8c181e51";
+		let context = "You are a world-class motivational coach, and I would like you to give words of affirmations.  Make sure you follow the user instructions.";
+		let prompt = "User instructions: Send me an affirmation for women only.  Include the affirmation only in your response, followed by a <br>, then add 'SheCodes AI' inside a <strong> element. ";
+		let url = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+		let affirmationElement = document.querySelector("#affirmation");
+		affirmationElement.classList.remove("hidden");
+		affirmationElement.innerHTML = `<div class="generating">⏳</div> generating your ${affirmationType} affirmation`;
+		axios.get(url).then(displayAffirmation);
 	}
 
 	else {
