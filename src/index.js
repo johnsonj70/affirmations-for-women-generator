797547453e61f6a9cae15b10e8f1db7cd97fb299
Janice <johnson.janice30@yahoo.co.uk>
2025-06-29 17:00:24 +0100

    Improved the UX

--- src/index.js
+++ src/index.js
@@ -18,12 +18,15 @@ function generateAffirmation(event) {
 	console.log(affirmationType);
 	
 	 if (affirmationType === "daily") {
-		
-		//build API url
+	
+	//build API url
 	let apiKey = "00a6bfb9b6053b4664t55oaa8c181e51";
 	let context = "You are a world-class motivational coach, and I would like you to give words of affirmations.  Make sure you follow the user instructions.";
 	let prompt = "User instructions: Send me an affirmation for women only.  Include the affirmation only in your response, followed by a <br>, then add 'SheCodes AI' inside a <strong> element. ";
 	let url = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+	let affirmationElement = document.querySelector("#affirmation");
+	affirmationElement.classList.remove("hidden");
+	affirmationElement.innerHTML = `<div class="generating">⏳</div> generating your ${affirmationType} affirmation`;
 	axios.get(url).then(displayAffirmation);
 		console.log("generating affirmation");
 	}
