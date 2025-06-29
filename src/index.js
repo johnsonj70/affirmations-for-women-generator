a5ed92762e891b519ed466b01c63510b3cc966f1
Janice <johnson.janice30@yahoo.co.uk>
2025-06-29 01:06:14 +0100

    Imported Axios and included API call

--- src/index.js
+++ src/index.js
@@ -1,13 +1,40 @@
-function generateAffirmation(event) {
-    event.preventDefault();
-
-    new Typewriter("#affirmation", {
-		strings: "I am worthy of love. I am proud of myself, and I am enough.",
+function displayAffirmation(response) {
+	console.log("affirmation generated");
+	//Display the generated affirmation
+	new Typewriter("#affirmation", {
+		strings: response.data.answer,
 		autoStart: true,
 		cursor: "",
 		delay: 0.5,
 	});
 }
+
+
+function generateAffirmation(event) {
+    event.preventDefault();
+
+	let affirmationType = document.querySelector("#user-instructions");
+	affirmationType = affirmationType.value;
+	console.log(affirmationType);
+	
+	 if (affirmationType === "daily") {
+		
+		//build API url
+	let apiKey = "00a6bfb9b6053b4664t55oaa8c181e51";
+	let context = "You are a world-class motivation coach, and I would like you to give words of affirmations";
+	let prompt = "Send me an affirmation for women only.  Include the affirmation only in your response.";
+	let url = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+	axios.get(url).then(displayAffirmation);
+		console.log("generating affirmation");
+	}
+
+	else {
+		alert("Please type 'daily' for an affirmation");
+		affirmationType = "";
+	}
+	
+}
+
 
 let affirmationFormElement = document.querySelector("#affirmation-generator-form");
 affirmationFormElement.addEventListener("submit", generateAffirmation);
