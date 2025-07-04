0dc6f4f2624b7cbde5aa200de9ef40723b1db510
Janice <johnson.janice30@yahoo.co.uk>
2025-07-04 14:09:26 +0100

    Changed text in prompt.

--- src/index.js
+++ src/index.js
@@ -15,25 +15,16 @@ function generateAffirmation(event) {
 	let affirmationType = document.querySelector("#user-instructions");
 	affirmationType = affirmationType.value;
 	
-	 if (affirmationType === "daily") {
-	
 	//build API url
 		let apiKey = "00a6bfb9b6053b4664t55oaa8c181e51";
 		let context = "You are a world-class motivational coach, and I would like you to give words of affirmations.  Make sure you follow the user instructions.";
-		let prompt = "User instructions: Send me an affirmation for women only.  Include the affirmation only in your response, followed by a <br>, then add 'SheCodes AI' inside a <strong> element. ";
+		let prompt = "User instructions: Send me an affirmation for women only.  Affirmation should be subject specific.  Include the affirmation and a love heart only in your response, followed by a <br>, then add 'SheCodes AI' inside a <strong> element. ";
 		let url = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 		let affirmationElement = document.querySelector("#affirmation");
 		affirmationElement.classList.remove("hidden");
 		affirmationElement.innerHTML = `<div class="generating">⏳</div> generating your ${affirmationType} affirmation ♥️`;
 		axios.get(url).then(displayAffirmation);
 	}
-
-	else {
-		alert("Please type 'daily' for an affirmation");
-		affirmationType = "";
-	}
-	
-}
 
 
 let affirmationFormElement = document.querySelector("#affirmation-generator-form");
